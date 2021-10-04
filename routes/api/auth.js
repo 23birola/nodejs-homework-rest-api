78631fa7cdba61fa8cfd350cfaef2cca86c0df5a
23birola <23birola@gmail.com>
2021-10-04 22:47:24 +0300

    added auth controllers

---
+++ routes/api/auth.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const { userJoiSchema } = require('../../models/users')
+const { controllerWrapper, validation, authenticate } = require('../../middlewares')
+const { auth: ctrl } = require('../../controllers/auth')
+
+const router = express.Router()
+
+/*
+1. Регистраця нового нового пользователя.
+2. Аутентификациия (логин) зарегистрированного пользователя.
+3. Авторизация аутентифицированного (зашедшего на сайт) пользователя.
+4. Выход (Logout).
+*/
+// api/auth/register
+router.post('/register', validation(userJoiSchema), controllerWrapper(ctrl.register))
+// router.post("/signup")
+
+router.post('/login', validation(userJoiSchema), controllerWrapper(ctrl.login))
+// router.post("/signin")
+
+router.get('/logout', authenticate, controllerWrapper(ctrl.logout))
+// router.get("/signuot")
+
+module.exports = router
